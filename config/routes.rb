ee0f9e7a40b5a65177467cfc9f51ca98d417bab4
David Yarhi <yarhi.david@gmail.com>
2022-02-17 21:44:59 +0900

    passing all rakes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "lists/new", to: "lists#new", as: :new_list
+  # get "lists", to: "lists#index"
+  # get "lists/:id", to: "lists#show", as: :list
+
+  # post "lists", to: "lists#create"
+
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new", as: :new_bookmark
+  # post "lists/:id/bookmarks", to: "bookmarks#create"
+
+  # delete "lists/:id/bookmarks", to: "bookmarks#destroy"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Removing old movies..."
+Movie.destroy_all
+
+puts "Creating new movies..."
+10.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    overview: Faker::Hipster.paragraph,
+    rating: rand(1..5)
+  )
+end
+
+puts "#{Movie.count} movies created."
